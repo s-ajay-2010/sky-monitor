3b6e737ce4f4dd18d7c0f1559ed345603705b809
s-ajay-2010 <ajaykrishnaasasikumar2020@gmail.com>
2026-05-23 16:43:39 +0530

    make the grid-lines

--- Sky-Monitor/src/App.jsx
+++ Sky-Monitor/src/App.jsx
@@ -5,7 +5,17 @@ export default function App() {
         width: "100vw",
         height: "100vh",
         background: "#001a00",
+
+        backgroundImage: `
+          linear-gradient(rgba(0, 255, 0, 0.15) 1px, transparent 1px),
+          linear-gradient(90deg, rgba(0, 255, 0, 0.15) 1px, transparent 1px)
+        `,
+        backgroundSize: "50px 50px",
+
+        position: "relative",
       }}
-    ></div>
-  )
+    >
+
+    </div>
+  );
 }
